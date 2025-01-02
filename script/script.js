3b96fe66b3dae10c99d2ccfc40e64f08c4783059
rafacostadev <rafaelcostamantis@gmail.com>
2025-01-01 22:06:23 -0300

    perfil de testes agora com referência a servidor local de testes

--- script/script.js
+++ script/script.js
@@ -1,11 +1,10 @@
-const url = "https://testesdeploy-production.up.railway.app/products";
+const url = "http://localhost:8080/products";
 
 async function chamarApi(){
     const answer = await fetch(url);
     if(answer.status === 200){
         const obj = await answer.json()
         dados = obj.content[0]
-        console.log(dados)
         document.getElementById("search-input").setAttribute("value", dados.name)
     }   
 }
